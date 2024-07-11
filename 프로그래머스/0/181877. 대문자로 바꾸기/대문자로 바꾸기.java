9ed06fe4581296ffb74154f7188b45a10dde9931
김이레 <165885725+elroi773@users.noreply.github.com>
2024-07-12 00:27:38 +0900

    [level 0] Title: 대문자로 바꾸기, Time: 0.03 ms, Memory: 70 MB -BaekjoonHub

---
+++ 프로그래머스/0/181877. 대문자로 바꾸기/대문자로 바꾸기.java
@@ -0,0 +1,7 @@
+class Solution {
+    public String solution(String myString) {
+        // 문자열을 대문자로 변환하여 answer에 저장
+        String answer = myString.toUpperCase();
+        return answer;
+    }
+}
