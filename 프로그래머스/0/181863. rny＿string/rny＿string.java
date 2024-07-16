985711acc9aa31995763e21a11fea3799d258726
김이레 <165885725+elroi773@users.noreply.github.com>
2024-07-16 09:36:24 +0900

    [level 0] Title: rny_string, Time: 0.08 ms, Memory: 72.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181863. rny＿string/rny＿string.java
@@ -0,0 +1,7 @@
+class Solution {
+    public String solution(String rny_string) {
+        // 'm'을 "rn"으로 대체하는 replace 메서드를 사용합니다.
+        String answer = rny_string.replace("m", "rn");
+        return answer;
+    }
+}
