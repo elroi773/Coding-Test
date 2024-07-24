4835679f83a58e5a9e46ac27b4e92fb89608f9c2
김이레 <165885725+elroi773@users.noreply.github.com>
2024-07-24 21:17:53 +0900

    [level 0] Title: 문자열을 정수로 변환하기, Time: 0.04 ms, Memory: 74.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181848. 문자열을 정수로 변환하기/문자열을 정수로 변환하기.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int solution(String n_str) {
+        // Use Integer.parseInt to convert the string to an integer
+        int answer = Integer.parseInt(n_str);
+        return answer;
+    }
+}
