862b6eb650a4d1c8debfa61b3adcc650bb970bdc
김이레 <165885725+elroi773@users.noreply.github.com>
2024-08-07 20:33:40 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.03 ms, Memory: 80.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        // Check if num1 is equal to num2
+        if (num1 == num2) {
+            return 1; // Return 1 if they are equal
+        } else {
+            return -1; // Return -1 if they are not equal
+        }
+    }
+}
