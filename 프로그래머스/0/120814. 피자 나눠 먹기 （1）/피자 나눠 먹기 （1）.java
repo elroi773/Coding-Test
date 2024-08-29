3441fe169ec11022343d1f445b0e03ee73c08571
김이레 <165885725+elroi773@users.noreply.github.com>
2024-08-30 08:15:31 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.03 ms, Memory: 79.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(int n) {
+        // 필요한 피자의 수를 계산합니다.
+        // (n + 6) / 7 는 (n을 7로 나눈 후 올림한 값)과 동일합니다.
+        int answer = (n + 6) / 7;
+        
+        return answer;
+    }
+}
