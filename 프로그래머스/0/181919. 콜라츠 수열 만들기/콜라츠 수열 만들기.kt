0288be266660bd550218d98b4ea991e7c3b7a6d1
김이레 <165885725+elroi773@users.noreply.github.com>
2025-06-30 23:07:43 +0900

    [level 0] Title: 콜라츠 수열 만들기, Time: 8.82 ms, Memory: 64.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.kt
@@ -0,0 +1,18 @@
+class Solution {
+    fun solution(n: Int): IntArray {
+        val result = mutableListOf<Int>()
+        var current = n
+
+        while (current != 1) {
+            result.add(current)
+            current = if (current % 2 == 0) {
+                current / 2
+            } else {
+                3 * current + 1
+            }
+        }
+
+        result.add(1) // 마지막 1 추가
+        return result.toIntArray()
+    }
+}
