313bea22b927308ab0b5de5b5658fe9182d40ea7
김이레 <165885725+elroi773@users.noreply.github.com>
2025-12-09 23:07:31 +0900

    [level 2] Title: 문자열 압축, Time: 2.29 ms, Memory: 7.67 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/README.md
+++ 프로그래머스/2/60057. 문자열 압축/README.md
@@ -1,10 +1,10 @@
 # [level 2] 문자열 압축 - 60057 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/60057?language=kotlin) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/60057?language=python) 
 
 ### 성능 요약
 
-메모리: 62.9 MB, 시간: 6.24 ms
+메모리: 7.67 MB, 시간: 2.29 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 12월 09일 23:05:38
+2025년 12월 09일 23:07:29
 
 ### 문제 설명
 
--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.py
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.py
@@ -1,31 +1,31 @@
 def solution(s):
-    # 예외: 길이가 1이면 압축 불가능 → 길이 1
+    # 길이가 1인 경우 바로 1 반환
     if len(s) == 1:
         return 1
-    
-    answer = float('inf')
 
-    # 1부터 s 길이의 절반까지 자르는 단위 길이를 검사
-    for cut in range(1, len(s) // 2 + 1):
+    answer = len(s)
+
+    # 1부터 len(s)//2까지 자르는 길이를 시도
+    for step in range(1, len(s) // 2 + 1):
         compressed = ""
-        prev = s[:cut]   # 첫 조각
+        prev = s[:step]   # 기준 문자열
         count = 1
 
-        # cut 단위로 문자열 쭉 비교
-        for i in range(cut, len(s), cut):
-            chunk = s[i:i+cut]
-            if chunk == prev:
+        # step 단위로 문자열 순회
+        for i in range(step, len(s), step):
+            # 이전 문자열과 동일하면 count 증가
+            if s[i:i + step] == prev:
                 count += 1
             else:
-                # 이전 반복 덩어리 추가
+                # 다르면 지금까지 결과 추가
                 compressed += (str(count) + prev) if count > 1 else prev
-                prev = chunk
+                prev = s[i:i + step]
                 count = 1
 
-        # 마지막 조각 처리
+        # 마지막 덩어리 처리
         compressed += (str(count) + prev) if count > 1 else prev
 
-        # 가장 짧은 길이 갱신
+        # 최솟값 갱신
         answer = min(answer, len(compressed))
 
     return answer
