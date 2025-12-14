4d24b58ddcdfcc2f0566a4b76c21fa1cb0248ccb
김이레 <165885725+elroi773@users.noreply.github.com>
2025-12-14 23:35:47 +0900

    [level 2] Title: 기능개발, Time: 0.04 ms, Memory: 7.68 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,7 +1,7 @@
 def solution(progresses, speeds):
     answer = []
     
-    # 1. 각 작업의 완료까지 필요한 날짜 계산
+    # 1. 각 작업 완료까지 걸리는 날짜 계산
     days = []
     for p, s in zip(progresses, speeds):
         days.append((100 - p + s - 1) // s)
@@ -12,15 +12,13 @@ def solution(progresses, speeds):
     
     for i in range(1, len(days)):
         if days[i] <= current_day:
-            # 이전 기능과 함께 배포
             count += 1
         else:
-            # 새로운 배포 시작
             answer.append(count)
             current_day = days[i]
             count = 1
     
-    # 마지막 배포 추가
+    # 마지막 배포
     answer.append(count)
     
     return answer
