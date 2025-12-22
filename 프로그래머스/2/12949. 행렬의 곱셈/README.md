63e4d4f6b5904de3349346cf2d10fb092b994c51
김이레 <165885725+elroi773@users.noreply.github.com>
2025-12-22 18:35:25 +0900

    [level 2] Title: 행렬의 곱셈, Time: 7.60 ms, Memory: 38.7 MB -BaekjoonHub

--- 프로그래머스/2/12949. 행렬의 곱셈/README.md
+++ 프로그래머스/2/12949. 행렬의 곱셈/README.md
@@ -1,10 +1,10 @@
 # [level 2] 행렬의 곱셈 - 12949 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12949?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12949?language=javascript) 
 
 ### 성능 요약
 
-메모리: 92.5 MB, 시간: 11.01 ms
+메모리: 38.7 MB, 시간: 7.60 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 12월 22일 18:34:17
+2025년 12월 22일 18:35:23
 
 ### 문제 설명
 
---
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.js
@@ -0,0 +1,16 @@
+function solution(arr1, arr2) {
+    const row = arr1.length;
+    const col = arr2[0].length;
+    const mid = arr2.length;
+
+    const answer = Array.from({ length: row }, () => Array(col).fill(0));
+
+    for (let i = 0; i < row; i++) {
+        for (let j = 0; j < col; j++) {
+            for (let k = 0; k < mid; k++) {
+                answer[i][j] += arr1[i][k] * arr2[k][j];
+            }
+        }
+    }
+    return answer;
+}
