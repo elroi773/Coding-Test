e8f0a19786f1a38a8bc7e6eae219244ed9a44d8d
김이레 <165885725+elroi773@users.noreply.github.com>
2025-12-23 21:26:19 +0900

    [level 2] Title: 하노이의 탑, Time: 9.23 ms, Memory: 17.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.py
@@ -0,0 +1,19 @@
+def solution(n):
+    answer = []
+
+    def hanoi(k, start, via, end):
+        if k == 1:
+            answer.append([start, end])
+            return
+        
+        # 1. n-1개를 보조 기둥으로
+        hanoi(k - 1, start, end, via)
+        
+        # 2. 가장 큰 원판 이동
+        answer.append([start, end])
+        
+        # 3. n-1개를 목표 기둥으로
+        hanoi(k - 1, via, start, end)
+
+    hanoi(n, 1, 2, 3)
+    return answer
