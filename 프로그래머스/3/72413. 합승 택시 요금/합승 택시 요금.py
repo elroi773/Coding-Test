b638ea25609be5dd6f37b92862166d73f21215fc
김이레 <165885725+elroi773@users.noreply.github.com>
2026-01-27 23:04:25 +0900

    [level 3] Title: 합승 택시 요금, Time: 766.15 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.py
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.py
@@ -1,11 +1,13 @@
 def solution(n, s, a, b, fares):
     INF = 10**18
+
+    # dist[i][j] = i -> j 최단거리
     dist = [[INF] * (n + 1) for _ in range(n + 1)]
     for i in range(1, n + 1):
         dist[i][i] = 0
 
+    # 양방향 간선
     for c, d, f in fares:
-        # 양방향
         if f < dist[c][d]:
             dist[c][d] = f
             dist[d][c] = f
@@ -14,9 +16,9 @@ def solution(n, s, a, b, fares):
     for k in range(1, n + 1):
         dk = dist[k]
         for i in range(1, n + 1):
-            if dist[i][k] == INF:
-                continue
             ik = dist[i][k]
+            if ik == INF:
+                continue
             di = dist[i]
             for j in range(1, n + 1):
                 nd = ik + dk[j]
