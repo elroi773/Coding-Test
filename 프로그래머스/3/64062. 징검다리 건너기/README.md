b6f12953ea3fbb6f1c99ee0a2520266f740362b3
김이레 <165885725+elroi773@users.noreply.github.com>
2026-02-08 18:22:30 +0900

    [level 3] Title: 징검다리 건너기, Time: 222.52 ms, Memory: 15.6 MB -BaekjoonHub

--- 프로그래머스/3/64062. 징검다리 건너기/README.md
+++ 프로그래머스/3/64062. 징검다리 건너기/README.md
@@ -1,10 +1,10 @@
 # [level 3] 징검다리 건너기 - 64062 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64062?language=kotlin) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64062?language=python) 
 
 ### 성능 요약
 
-메모리: 72.2 MB, 시간: 42.37 ms
+메모리: 15.6 MB, 시간: 222.52 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 02월 08일 18:20:54
+2026년 02월 08일 18:22:28
 
 ### 문제 설명
 
--- 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.py
+++ 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.py
@@ -1,9 +1,11 @@
 def solution(stones, k):
-    # m명이 건널 수 있는지 체크
+    # m명이 건널 수 있는지 판별
+    # stones[i] < m 이면 m번째 사람 기준으로 그 돌은 0 이하가 되어 밟을 수 없음
+    # 밟을 수 없는 돌이 연속 k개 이상이면 실패
     def can_cross(m):
         consecutive = 0
         for s in stones:
-            if s < m:               # m명이 지나가면 0 이하가 되는 돌(밟기 불가)
+            if s < m:
                 consecutive += 1
                 if consecutive >= k:
                     return False
@@ -17,8 +19,9 @@ def solution(stones, k):
     while left <= right:
         mid = (left + right) // 2
         if can_cross(mid):
-            answer = mid          # mid명 가능 -> 더 큰 값 시도
+            answer = mid
             left = mid + 1
         else:
-            right = mid - 1       # mid명 불가능 -> 더 작은 값으로
+            right = mid - 1
+
     return answer
