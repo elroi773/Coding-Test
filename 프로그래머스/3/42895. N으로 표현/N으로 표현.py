241beb475f808a092dc4d48fb1b99df626ec772c
김이레 <165885725+elroi773@users.noreply.github.com>
2026-02-16 20:52:14 +0900

    [level 3] Title: N으로 표현, Time: 14.81 ms, Memory: 8.55 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.py
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.py
@@ -2,14 +2,13 @@ def solution(N, number):
     if N == number:
         return 1
 
-    dp = [set() for _ in range(9)]  # 0은 안 쓰고 1~8 사용
+    dp = [set() for _ in range(9)]  # dp[i] = N을 i번 사용해서 만들 수 있는 값들 (1~8)
 
     for i in range(1, 9):
-        # 이어붙인 숫자 (ex: 5, 55, 555 ...)
-        concat = int(str(N) * i)
-        dp[i].add(concat)
+        # 이어붙인 수: N, NN, NNN...
+        dp[i].add(int(str(N) * i))
 
-        # j + (i-j)로 쪼개서 사칙연산 조합
+        # j + (i-j)로 분할해서 사칙연산 조합
         for j in range(1, i):
             for a in dp[j]:
                 for b in dp[i - j]:
@@ -17,7 +16,7 @@ def solution(N, number):
                     dp[i].add(a - b)
                     dp[i].add(a * b)
                     if b != 0:
-                        dp[i].add(a // b)  # 나머지 버림
+                        dp[i].add(a // b)  # 나머지 무시
 
         if number in dp[i]:
             return i
--- 프로그래머스/3/42895. N으로 표현/README.md
+++ 프로그래머스/3/42895. N으로 표현/README.md
@@ -1,10 +1,10 @@
 # [level 3] N으로 표현 - 42895 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42895?language=kotlin) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42895?language=python) 
 
 ### 성능 요약
 
-메모리: 64.9 MB, 시간: 25.36 ms
+메모리: 8.55 MB, 시간: 14.81 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 02월 16일 20:51:17
+2026년 02월 16일 20:52:13
 
 ### 문제 설명
 
