fe285e7117926ccabc695b4b8e6182d5503a65ee
김이레 <165885725+elroi773@users.noreply.github.com>
2026-03-28 20:05:45 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 168 ms, Memory: 17672 KB -BaekjoonHub

---
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        long A = sc.nextLong();
+        long B = sc.nextLong();
+        long V = sc.nextLong();
+
+        long day = (V - A + (A - B) - 1) / (A - B) + 1;
+        System.out.println(day);
+    }
+}
